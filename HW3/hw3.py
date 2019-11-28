2d862b921754d5ae52daf0c46121efd45dd5a391
yiugetan <yasamnono@gmail.com>
2019-11-28 18:06:45 +0800

    key convert

--- HW3/hw3.py
+++ HW3/hw3.py
@@ -2,6 +2,24 @@ from Crypto.Cipher import AES
 from PIL import Image
 from sys import argv
 
+def pad(text):
+	padding = 16 - (len(text) % 16)
+	return text + bytes([padding] * padding)
+
+def unpad(text):
+	padding = text[-1]
+	return text[:-padding]
+
+def AES_encrypt_one_block(plain,key):
+    cipher = AES.new(key, AES.MODE_ECB)
+    ciphertext = cipher.encrypt(plain)
+    return ciphertext
+
+def AES_decrypt_one_block(cipher,key):
+    plain = AES.new(key, AES.MODE_ECB)
+    plaintext = plain.encrypt(cipher)
+    return plaintext
+
 class AESppm():
     def __init__(self,ppm_bin):
         string = ppm_bin.readline()
@@ -16,11 +34,25 @@ class AESppm():
         return to_return
 
     def ecb(self,key,en_de) : #0=encrypt 1=decrypt
-        if en_de==0:
-            pass
+        pass
 
     def cbc(self,key,en_de) :
-        pass
+        to_return = self
+        vector = [key]
+        to_return.pixels_bin=bytes()
+        processed_block=bytes()
+        for i in range(int(len(self.pixels_bin)/16)) :
+            block=self.pixels_bin[16*i:16*i+16]
+            block ^=vector[i]
+            if(en_de==0) :
+                processed_block+=AES_encrypt_one_block(block,key)
+            else :
+                processed_block+=AES_decrypt_one_block(block,key)
+            vector.append(processed_block)
+            to_return.pixels_bin+=processed_block
+        return to_return
+            
+
     def my(self,key,en_de) :
         pass
     
@@ -37,18 +69,17 @@ if __name__ == "__main__":
     input_jpg_path = argv[1]
     en_de = argv[2]
     mode = argv[3]
-    key = argv[4]
+    key = bytes.fromhex(argv[4])
 
     with Image.open(input_jpg_path) as input_png_file :
         input_ppm_path = input_jpg_path[:-4]+".ppm"
         input_png_file.save(input_ppm_path)
     with open(input_ppm_path,'rb') as input_ppm_bin :
         ppm_file = AESppm(input_ppm_bin)
-        ppm_file.ecb(key,0)
+        # ppm_file=ppm_file.cbc(key,0)
         output_ppm_path = '/'.join(input_ppm_path.split('/')[:-1])+"/output.ppm"
         with open(output_ppm_path,'wb') as output_ppm_bin :
             output_ppm_bin.write(bytes(ppm_file))
         output_jpg_path = output_ppm_path[:-4]+".jpg"
         with Image.open(output_ppm_path) as output_ppm :
             output_ppm.save(output_jpg_path,"png")
-
