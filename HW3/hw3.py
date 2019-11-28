edf1a60c8676c2311b9248f35639e53d01162858
yiugetan <yasamnono@gmail.com>
2019-11-28 15:08:22 +0800

    jpg output

--- HW3/hw3.py
+++ HW3/hw3.py
@@ -9,13 +9,13 @@ class AESppm():
         self.length = int(string.split()[0])
         self.width = int(string.split()[1])
         self.depth = int(ppm_bin.readline()) 
-        self.pixels_bin = bytes()
-        lines = ppm_bin.readlines()
-        for line in lines:
-            self.pixels_bin += line
+        self.pixels_bin = ppm_bin.readlines()
     
     def __bytes__(self):
-        return bytes("P6\n","utf-8")+bytes(str(self.length),'utf-8')+bytes(" ","utf-8")+bytes(str(self.width),'utf-8')+bytes("\n","utf-8")+bytes(str(self.depth),'utf-8')+bytes("\n","utf-8")+bytes(self.pixels_bin)
+        to_return = bytes("P6\n","utf-8")+bytes(str(self.length),'utf-8')+bytes(" ","utf-8")+bytes(str(self.width),'utf-8')+bytes("\n","utf-8")+bytes(str(self.depth),'utf-8')+bytes("\n","utf-8")
+        for line in self.pixels_bin:
+            to_return+= bytes(line)
+        return to_return
 
     def ecb(self,key,en_de) : #0=encrypt 1=decrypt
         pass
@@ -39,14 +39,16 @@ if __name__ == "__main__":
     mode = argv[3]
     key = argv[4]
 
-    with Image.open(input_jpg_path) as input_jpg_file :
+    with Image.open(input_jpg_path) as input_png_file :
         input_ppm_path = input_jpg_path[:-4]+".ppm"
-        input_jpg_file.save(input_ppm_path)
+        input_png_file.save(input_ppm_path)
     with open(input_ppm_path,'rb') as input_ppm_bin :
         ppm_file = AESppm(input_ppm_bin)
         ppm_file.ecb(key,0)
         output_ppm_path = '/'.join(input_ppm_path.split('/')[:-1])+"/output.ppm"
-        with open(output_ppm_path,'wb') as output_ppm :
-            output_ppm.write(bytes(ppm_file))
-        
-        #with Image.open(output_jpg_path)
+        with open(output_ppm_path,'wb') as output_ppm_bin :
+            output_ppm_bin.write(bytes(ppm_file))
+        output_jpg_path = output_ppm_path[:-4]+".jpg"
+        with Image.open(output_ppm_path) as output_ppm :
+            output_ppm.save(output_jpg_path,"png")
+
