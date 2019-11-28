16dbec23986002e7cb7b4215762c30e03eb34a4c
yiugetan <yasamnono@gmail.com>
2019-11-28 12:07:13 +0800

    hw3 commit2

--- HW3/hw3.py
+++ HW3/hw3.py
@@ -10,14 +10,17 @@ class AESppm():
         self.width = int(string.split()[1])
         self.depth = int(ppm_bin.readline())
         self.pixels_bin = ppm_bin.readline()
-        
+    
+    def __bytes__(self):
+        return bytes("P6\n","utf-8")+bytes(self.length)+bytes(" ","utf-8")+bytes(self.width)+bytes("\n","utf-8")+bytes(self.depth)+bytes("\n","utf-8")+bytes(self.pixels_bin)
+
     def ecb(self,key,en_de) : #0=encrypt 1=decrypt
         pass
     def cbc(self,key,en_de) :
         pass
     def my(self,key,en_de) :
         pass
-
+    
 
 
 #cipher = AES.new(key, AES.MODE_ECB)
@@ -28,17 +31,17 @@ if __name__ == "__main__":
     if(len(argv)!=5):
         print("Arguments error")
         exit()
-    filepath = argv[1]
+    input_path = argv[1]
     en_de = argv[2]
     mode = argv[3]
     key = argv[4]
 
-    with Image.open(filepath) as image_file :
-        ppm_path = filepath[:-4]+".ppm"
+    with Image.open(input_path) as image_file :
+        ppm_path = input_path[:-4]+".ppm"
         image_file.save(ppm_path)
     with open(ppm_path,'rb') as ppm_bin :
         ppm_file = AESppm(ppm_bin)
         ppm_file.ecb(key,0)
-        output_path = 
-        with 
-
+        output_path = '/'.join(ppm_path.split('/')[:-1])+"/output.ppm"
+        with open(output_path,'wb') as output_ppm :
+            output_ppm.write(bytes(ppm_file))
